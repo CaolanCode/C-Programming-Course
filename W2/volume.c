057415bfd0b67bba3fdf0ad7e3ca2bd53fa169e9
CaolanCode <cmacmahon3@gmail.com>
2022-07-24 13:44:30 +0100

    added files from week 2

---
+++ W2/volume.c
@@ -0,0 +1,15 @@
+/*
+ * Peer review test
+ * July, 23, 2022
+ */
+
+#include <stdio.h>
+#define PI 3.14159
+
+int main(void){
+	double radius;
+	printf("Enter radius:");
+	scanf("%lf", &radius);
+	printf("volume is : %lf \n\n", 4*radius*radius*radius/3);
+	return 0;
+}
