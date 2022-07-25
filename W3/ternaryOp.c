5820653ebc13378b2cee51bce792a5b45b4e39b1
CaolanCode <cmacmahon3@gmail.com>
2022-07-25 11:45:19 +0100

    added week 3 programs

---
+++ W3/ternaryOp.c
@@ -0,0 +1,29 @@
+/*
+ * ternary operator and flow of control
+ * CaolanCode
+ * July, 24, 2022
+ */
+
+#include <stdio.h>
+
+int main(void){
+	int speed;
+	printf("\nEnter your speed as an integer:");
+	scanf("%d", &speed);
+	speed = (speed <= 65)? (65) :(speed <= 70)? (70): (90); // check if input is below 65, if true it returns 65. If not, it checks if the speed is below 70, if true it returns 70. If not, it return 90
+	switch (speed){
+		case 65: 
+			printf("\nNo speeding ticket\n\n");
+			break;
+		case 70:
+			printf("\nSpeeding ticket\n\n");
+			break;
+		case 90:
+			printf("\nExpensive Speeding ticket\n\n");
+			break;
+		default:
+			printf("\nIncorrect speed\n\n");
+	}
+	return 0;
+}
+			
